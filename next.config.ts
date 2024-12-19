c615f8ad20ec072d9e20352f151ec04a42409b67
Maria Luque Anguita <mluqueanguita@gmail.com>
2024-12-19 17:46:22 +0100

    add procfile

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    PORT: process.env.PORT || '3000'
+  }
 };
 
 export default nextConfig;
