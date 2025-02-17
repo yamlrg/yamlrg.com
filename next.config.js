7eaea57d468eb486238cf03c937a43b9cde4cfce
Maria Luque Anguita <mluqueanguita@gmail.com>
2025-02-17 10:22:26 -0800

    remove rewrites

--- next.config.js
+++ next.config.js
@@ -43,14 +43,6 @@ const nextConfig = {
       }
     ];
   },
-  rewrites: async () => {
-    return [
-      {
-        source: '/api/:path*',
-        destination: '/api/:path*',
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig; 
